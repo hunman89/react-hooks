afd19efba0825a6957838493848475fcc0ad840a
hunman89 <hunman89@gmail.com>
2021-05-22 16:07:21 +0900

    add. useScroll

--- src/index.js
+++ src/index.js
@@ -1,34 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-const useNetwork = (onChange) => {
-  const [status, setStatus] = useState(navigator.onLine);
-  const handleChange = () => {
-    if (typeof onChange == "function") {
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
+const useScroll = () => {
+  const [state, setstate] = useState({ x: 0, y: 0 });
+  const onScroll = () => {
+    setstate({ y: window.scrollY, x: window.scrollX });
   };
-
   useEffect(() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
+    window.addEventListener("scroll", onScroll);
     return () => {
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
+      window.removeEventListener("scroll", onScroll);
     };
   });
-  return status;
+  return state;
 };
 
 const App = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "online" : "offline");
-  };
-  const onLine = useNetwork(handleNetworkChange);
+  const { y } = useScroll();
   return (
-    <div className="App">
-      <h1>{onLine ? "Online" : "offline"}</h1>
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
     </div>
   );
 };
