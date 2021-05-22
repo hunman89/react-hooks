d9d1703b2417abcf4216ec40c7a9fcbf628717f4
hunman89 <hunman89@gmail.com>
2021-05-22 13:59:39 +0900

    add. useFadeIn

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const useBeforeLeave = (onBefore) => {
-  const handle = (event) => {
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
-    }
-  };
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-    return () => {
-      document.removeEventListener("mouseleave", handle);
-    };
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
   });
-  if (typeof onBefore != "function") {
+  if (typeof duration != "number" || typeof delay != "number") {
     return;
   }
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => {
-    console.log("please don't leave");
-  };
-  useBeforeLeave(begForLife);
-  return <div className="App"></div>;
+  const fadeInH1 = useFadeIn(2, 1);
+  const fadeInP = useFadeIn(5, 3);
+  return (
+    <div className="App">
+      <h1 {...fadeInH1}>hello</h1>
+      <p {...fadeInP}>lorem ipsum djfdiasjfieownefk</p>
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("root");
